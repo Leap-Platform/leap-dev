5a4b489d0d4c3f8d773a954df886e3b1a4d19c26
Aravind G S <aravind@jiny.io>
2022-06-27 17:37:12 +0530

    Corrected Package.swift

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         .binaryTarget(name: "LeapSDK", path: "LeapSDK.xcframework"),
         .binaryTarget(
             name: "LeapCoreSDK", 
-            url: "https://github.com/Leap-Platform/leap-core-dev/releases/download/2.3.1/LeapCoreSDK.xcframework.zip",
-            checksum: "${new_checksum}"
+            url: "https://github.com/Leap-Platform/leap-core-dev/releases/download/2.0.64/LeapCoreSDK.xcframework.zip",
+            checksum: "3624ca86c7681fd4df0cf8864d5d91c1039dd63de6bee64198f4fa2837965366"
         )
     ]
 )
